b8ed56dbeaab41deaec12be307d3e1959d015312
Jainish Shah <jainish.shah@workday.com>
2019-05-11 18:25:36 -0700

    added storage api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var oauthRouter = require('./routes/oauth');
 var pingRouter = require('./routes/ping');
 var systemRouter = require('./routes/system');
+var storageRouter = require('./routes/storage');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/ping', pingRouter);
 app.use('/oauth', oauthRouter);
 app.use('/system', systemRouter);
+app.use('/storage', storageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/storage.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var request = require('request');
+
+var router = express.Router();
+var artifactory = require('../artifactory.json');
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+router.post('/', function(req, res) {
+  request({
+    url: artifactory.artifactoryURL + "/api/storageinfo", //URL to hit
+    method: 'GET', //Specify the method
+    auth: {
+      'user': artifactory.artifactoryUsername,
+      'pass': artifactory.artifactoryPassword
+    }
+}, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        res.json(body);
+
+    }
+})
+});
+
+module.exports = router;
