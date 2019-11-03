7b8315a30a48daedb471090a61aeafb8c122d6a3
Jainish Shah <jainish.shah@workday.com>
2019-11-03 12:42:26 -0800

    new subcommands

--- features/slash_commands.js
+++ features/slash_commands.js
@@ -5,6 +5,8 @@
 const {SlackDialog} = require('botbuilder-adapter-slack');
 var artCli = require('artifactory-client-node');
 var art = new artCli.artifactory();
+var repositories = new artCli.repositories();
+var security = new artCli.security();
 
 module.exports = function (controller) {
 
@@ -17,34 +19,48 @@ module.exports = function (controller) {
     });
 
     controller.on('slash_command', async (bot, message) => {
-        if (message.text === 'plain') {
-            await bot.reply(message, 'This is a plain reply');
-        } else if (message.text === 'public') {
-            await bot.replyPublic(message, 'This is a public reply');
-        } else if (message.text === 'private') {
-            await bot.replyPrivate(message, 'This is a private reply');
-        } else if (message.text === 'help') {
+        if (message.text === 'help') {
             await bot.replyPrivate(message, 'Here to help');
         } else if (message.text === 'ping') {
             var ping = art.ping();
             ping.then(function (result) {
-                bot.replyPrivate(message, result);
+                bot.replyPrivate(message, `Artifactory status is: ${result}`);
             }, function (err) {
                 console.log(err);
             });
         } else if (message.text === 'system') {
             var system = art.system();
             system.then(function (result) {
-                bot.replyPrivate(message, result);
+                bot.replyPrivate(message, `${result}`);
+            }, function (err) {
+                console.log(err);
+            });
+        } else if (message.text === 'version') {
+            var version = art.getVersion();
+            version.then(function (result) {
+                bot.replyPrivate(message, `${result}`);
+            }, function (err) {
+                console.log(err);
+            });
+        } else if (message.text === 'storage') {
+            var storage = art.getStorage();
+            storage.then(function (result) {
+                bot.replyPrivate(message, `${result}`);
+            }, function (err) {
+                console.log(err);
+            });
+        } else if (message.text === 'getApiKey') {
+            var getApiKey = security.getApiKey();
+            getApiKey.then(function (result) {
+                bot.replyPrivate(message, `${result}`);
             }, function (err) {
                 console.log(err);
             });
         }
 
-
-
         // set http status
         // bot.httpBody({text:'You can send an immediate response using bot.httpBody()'});
+        // bot.httpBody({text:'Executed command: ' + `${message.text}`});
         bot.httpStatus(200);
 
     });
